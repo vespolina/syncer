ff178a827afc739ed3b0aa46aeb801ec0baef097
Luis Cordova <cordoval@gmail.com>
2013-11-21 19:17:28 -0500

    clean a bit tests with a base class for the setup

--- tests/Vespolina/Tests/Sync/Functional/SyncEntitiesWithDependencyTest.php
+++ tests/Vespolina/Tests/Sync/Functional/SyncEntitiesWithDependencyTest.php
@@ -9,40 +9,8 @@
 
 namespace Vespolina\Tests\Sync\Functional;
 
-use Monolog\Logger;
-use Monolog\Handler\TestHandler;
-use Symfony\Component\Yaml\Parser;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Vespolina\Sync\Gateway\SyncMemoryGateway;
-use Vespolina\Sync\Manager\SyncManager;
-
-class SyncEntitiesWithDependencyTest extends \PHPUnit_Framework_TestCase
+class SyncEntitiesWithDependencyTest extends SyncBaseTestCase
 {
-    protected $dispatcher;
-    protected $gateway;
-    protected $logHandler;
-    protected $manager;
-    protected $remoteServiceAdapter;
-
-    protected function setUp()
-    {
-        $this->dispatcher = new EventDispatcher();
-        $this->gateway = new SyncMemoryGateway();
-
-        $this->logHandler = new TestHandler();
-        $logger = new Logger('test', array($this->logHandler));
-
-        $yamlParser = new Parser();
-        $config = $yamlParser->parse(file_get_contents(__DIR__ . '/single_entity.yml'));
-        $this->manager = new SyncManager($this->gateway, $this->dispatcher, $logger, $config['syncer']);
-
-        // Create a remote service adapter which can deal with products
-        $this->remoteServiceAdapter = new SimpleRemoteServiceAdapter(array('category', 'product'));
-        $this->remoteServiceAdapter->setupFakeData();
-
-        $this->manager->addServiceAdapter($this->remoteServiceAdapter);
-    }
-
     public function testSyncEntities()
     {
         $state = $this->manager->getState('product');
