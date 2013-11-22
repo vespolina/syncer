119ce986dd5d333fc9427eb399280a3d353cf22c
Luis Cordova <cordoval@gmail.com>
2013-11-21 22:27:46 -0500

    plug generation of schema

--- tests/Vespolina/Tests/Sync/Gateway/SyncDoctrineORMGatewayTest.php
+++ tests/Vespolina/Tests/Sync/Gateway/SyncDoctrineORMGatewayTest.php
@@ -12,6 +12,7 @@ namespace Vespolina\Tests\Sync\Gateway;
 use Doctrine\Common\Persistence\Mapping\Driver\SymfonyFileLocator;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\Driver\XmlDriver;
+use Doctrine\ORM\Tools\SchemaTool;
 use Doctrine\ORM\Tools\Setup;
 use Vespolina\Sync\Gateway\SyncDoctrineORMGateway;
 
@@ -23,8 +24,7 @@ class SyncDoctrineORMGatewayTest extends SyncGatewayTestCommon
             'driver'   => 'pdo_sqlite',
             'user'     => 'root',
             'password' => '',
-            'dbname'   => 'foo',
-            'path'     => __DIR__.'/fixture.sqlite',
+            'path'     => __DIR__.'/fixture.db',
         );
 
         $locatorXml = new SymfonyFileLocator(
@@ -37,8 +37,28 @@ class SyncDoctrineORMGatewayTest extends SyncGatewayTestCommon
         $config = Setup::createConfiguration();
         $config->setMetadataDriverImpl(new XmlDriver($locatorXml));
         $em = EntityManager::create($dbParams, $config);
+        $this->generateSchema($em);
         $this->gateway = new SyncDoctrineORMGateway($em, 'Vespolina\Entity\Action\Action');
 
         parent::setUp();
+    }
+
+    protected function generateSchema($em)
+    {
+        // Get the metadata from the application to create the schema.
+        $metadatas = $this->getMetadatas($em);
+
+        if (!empty($metadatas)) {
+            // Create SchemaTool
+            $tool = new SchemaTool($em);
+            $tool->createSchema($metadatas);
+        } else {
+            throw \Exception('No Metadata Classes to process.');
+        }
+    }
+
+    protected function getMetadatas($em)
+    {
+        return $em->getMetadataFactory()->getAllMetadata();
     }
 }
